7746157c641ede382b81f9911b3fed1fed0f642e
Oshintha Sandaruwan <155742370+Oshintha@users.noreply.github.com>
2024-12-17 18:15:38 +0200

    Update client.c

--- client.c
+++ client.c
@@ -6,52 +6,81 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 23:39:10 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/11/29 23:39:10 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/12/13 13:57:43 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include "libft/ft_printf.h"
 #include <signal.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void	send_signal(int pid, unsigned char character)
 {
-	int				i;
+	int	i;
 
 	i = 8;
 	while (i > 0)
 	{
 		i--;
+		// Check the current bit and send the corresponding signal
 		if ((character >> i) % 2 == 0)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
-		usleep(42);
+		usleep(100); // Slightly larger delay for reliability
 	}
 }
-int	main(int ac, char **av[])
-{
-	int			i;
 
-	i=0;
-	if (argc != 3)
+int	validate_pid(const char *pid_str)
+{
+	int	i;
+
+	i = 0;
+	while (pid_str[i])
 	{
-		ft_printf("Error: Try again with correct format"); //if needed,  exit(ft_printf("Error: Try again with correct format"))
-		ft_printf("Usage: %s <pid> <message>\n", argv[0]);
-		exit(0);
-	}
-	while (av[1][i])
-	{
-		if(av[1][i] < '0' || av[i][i] > '9')
-			exit(ft_printf("Error: Try again with correct PID"));
+		// Ensure all characters in PID string are digits
+		if (pid_str[i] < '0' || pid_str[i] > '9')
+		{
+			ft_printf("Error: Invalid PID format. Please use numeric PID.\n");
+			exit(EXIT_FAILURE);
+		}
 		i++;
 	}
-	message = argv[2];
+	return (ft_atoi(pid_str));
+}
+
+void	print_usage_and_exit(const char *program_name)
+{
+	ft_printf("Usage: %s <PID> <MESSAGE>\n", program_name);
+	exit(EXIT_FAILURE);
+}
+
+int	main(int ac, char **av)
+{
+	int		pid;
+	char	*message;
+	int		i;
+
+	if (ac != 3)
+	{
+		ft_printf("Error: Incorrect number of arguments.\n");
+		print_usage_and_exit(av[0]);
+	}
+
+	// Validate and parse PID
+	pid = validate_pid(av[1]);
+
+	// Get the message from arguments
+	message = av[2];
 	i = 0;
 	while (message[i])
-	send_signal(ft_atoi(av[1]), message[i++]);
-	send_signal(ft_atoi(av[1]), '\0');
-    send_signal(ft_atoi(av[1]), '\n');
-    return (0);
+		send_signal(pid, message[i++]);
+
+	// Send a null byte and newline to indicate end of message
+	send_signal(pid, '\0');
+	send_signal(pid, '\n');
+	return (0);
 }
