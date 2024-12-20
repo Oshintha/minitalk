64949c046a1a4bb108c4e8bb3708b4506a26f4fa
Oshintha Sandaruwan <155742370+Oshintha@users.noreply.github.com>
2024-12-20 18:14:35 +0200

    Update client.c

--- client.c
+++ client.c
@@ -6,16 +6,21 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 23:39:10 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/12/13 13:57:43 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/12/20 14:55:02 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "libft/ft_printf.h"
 #include <signal.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+volatile sig_atomic_t	g_ack_received = 0;
+
+void	ft_error_client(const char *message)
+{
+	ft_printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
 
 void	send_signal(int pid, unsigned char character)
 {
@@ -25,62 +30,74 @@ void	send_signal(int pid, unsigned char character)
 	while (i > 0)
 	{
 		i--;
-		// Check the current bit and send the corresponding signal
-		if ((character >> i) % 2 == 0)
-			kill(pid, SIGUSR2);
+		if ((character >> i) & 1)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				ft_error_client("Error PID: Failed to send SIGUSR1");
+		}
 		else
-			kill(pid, SIGUSR1);
-		usleep(100); // Slightly larger delay for reliability
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				ft_error_client("Error PID: Failed to send SIGUSR2");
+		}
+		g_ack_received = 0;
+		usleep(500);
+		if (g_ack_received == -1)
+		{
+			ft_error_client("Error: Server reported an issue with the signal.");
+			g_ack_received = 0;
+		}
 	}
 }
 
 int	validate_pid(const char *pid_str)
 {
 	int	i;
+	int	pid;
 
 	i = 0;
 	while (pid_str[i])
 	{
-		// Ensure all characters in PID string are digits
 		if (pid_str[i] < '0' || pid_str[i] > '9')
-		{
-			ft_printf("Error: Invalid PID format. Please use numeric PID.\n");
-			exit(EXIT_FAILURE);
-		}
+			ft_error_client("Error: Invalid PID format. Use numeric PID.");
 		i++;
 	}
-	return (ft_atoi(pid_str));
+	pid = ft_atoi(pid_str);
+	if (pid <= 0)
+		ft_error_client("Error: PID must be a positive integer.");
+	return (pid);
 }
 
-void	print_usage_and_exit(const char *program_name)
+static void	signal_handler(int signal)
 {
-	ft_printf("Usage: %s <PID> <MESSAGE>\n", program_name);
-	exit(EXIT_FAILURE);
+	if (signal == SIGUSR1)
+		g_ack_received = 1;
 }
 
 int	main(int ac, char **av)
 {
-	int		pid;
-	char	*message;
-	int		i;
+	struct sigaction	sa;
+	int					pid;
+	char				*message;
+	int					i;
 
 	if (ac != 3)
 	{
-		ft_printf("Error: Incorrect number of arguments.\n");
-		print_usage_and_exit(av[0]);
+		ft_printf("Usage: %s <PID> <MESSAGE>\n", av[0]);
+		ft_error_client("Error: Incorrect number of arguments.");
 	}
-
-	// Validate and parse PID
 	pid = validate_pid(av[1]);
-
-	// Get the message from arguments
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = signal_handler;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+		ft_error_client("Error: Failed to set up signal handlers.");
 	message = av[2];
 	i = 0;
 	while (message[i])
 		send_signal(pid, message[i++]);
-
-	// Send a null byte and newline to indicate end of message
 	send_signal(pid, '\0');
-	send_signal(pid, '\n');
+	ft_printf("Message sent successfully to PID %d\n", pid);
 	return (0);
 }
